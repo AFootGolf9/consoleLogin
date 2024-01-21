5d69359a564cd1907b46c0e6f99521cb0739cf35
AFootGolf9 <adriandosanjos000@gmail.com>
2024-01-21 16:34:26 -0300

    update

--- User.py
+++ User.py
@@ -1,4 +1,20 @@
 import pymongo
 
-def createUser():
-    
+def createUser(collection, name, password):
+    """
+    This function allows a user to sign in by entering their username, password, and a welcome message. 
+    If the username already exists in the database, the function prompts the user to enter a different username.
+    """
+    result = collection.find_one({"name" : name})
+    if result  is None:
+        password = password.encode("utf-8")
+        password = hashlib.sha256(password)
+        message  = input("Write a welcome message : ")
+        document = {
+            "name" : name,
+            "password" : password.hexdigest(),
+            "message" : message
+        }
+        collection.insert_one(document)
+    else:
+        print("The user already exists, try another Username :")
